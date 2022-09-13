8893215a5d1748988437ca50265917a827b9d03b
Vatnehol Sindre <sindre.vatnehol@hi.no>
2022-09-13 13:57:29 +0200

    fix

--- R/AutoFigures.R
+++ R/AutoFigures.R
@@ -115,7 +115,7 @@ plot_progression <- function(projectPath,doc){
     gg_plot<-ggplot2::ggplot(data=stratum,ggplot2::aes(x=x,y=y,group=StratumName))+
       ggplot2::geom_polygon(colour='black',fill='red',alpha=0.4)+
       ggplot2::geom_point(data=data[[1]]$Log,ggplot2::aes(x=Longitude,y=Latitude,colour=DateTime,group=NULL),size=0.1)+
-      ggplot2::xlab('Longitude')+ggplot2::ylab('Latitude')+ ggplot2::theme(panel.background = element_blank())+
+      ggplot2::xlab('Longitude')+ggplot2::ylab('Latitude')+ ggplot2::theme(panel.background = ggplot2::element_blank())+
       ggplot2::theme(legend.position = "bottom",
             legend.key.width=unit(0.1,"npc"))
 
@@ -160,7 +160,7 @@ plot_NASC <- function(projectPath,doc=NULL){
       ggplot2::geom_polygon(colour='black',fill='red',alpha=0.4)+
       ggplot2::geom_point(data=data,ggplot2::aes(x=Longitude,y=Latitude,group=NULL,size=NASC,colour=NASC),alpha=0.1)+
       ggplot2::geom_point(data=data,ggplot2::aes(x=Longitude,y=Latitude,group=NULL),size=0.1)+
-      viridis::scale_color_viridis(option = "plasma")+ ggplot2::theme(panel.background = element_blank())+
+      viridis::scale_color_viridis(option = "plasma")+ ggplot2::theme(panel.background = ggplot2::element_blank())+
       ggplot2::xlab('Longitude')+ggplot2::ylab('Latitude')
 
     if(!is.null(doc)){
